32685ec92089a0f2b0b5e6b70ba2550fd9b8f055
quyet <phamquyet9688@gmail.com>
2022-12-01 12:20:51 +0700

    case

--- ball.js
+++ ball.js
@@ -1,5 +1,5 @@
 class Ball {
-    constructor(x,y,radius,color) {
+    constructor(x, y, radius, color) {
         this.x = x;
         this.y = y;
         this.radius = radius;
@@ -7,14 +7,15 @@ class Ball {
         this.canvas = document.getElementById('my-canvas');
         this.ctx = this.canvas.getContext('2d');
     }
+
+    move() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
     draw() {
         this.ctx.beginPath();
         this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2)
         this.ctx.fillStyle = this.color
         this.ctx.fill();
-    }
-
-    move() {
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
--- main.js
+++ main.js
@@ -2,27 +2,32 @@ let canvas= document.getElementById("my-canvas");
 let ctx= canvas.getContext("2d");
 
 
-let ball = new Ball(175, 100, 20, 'red')
-let dx=2;
-let dy=2;
+let ball = new Ball(100, 100, 20, 'red')
+let dx=10;
+let dy=5;
 let score =document.getElementById("scores");
+
 function ballmove1() {
-    if (ball.x < ball.radius || ball.x < canvas.width - ball.radius) {
+    if (ball.x < ball.radius || ball.x > canvas.width - ball.radius) {
         dx = -dx;
     }
     if (ball.y < ball.radius) {
         dy = -dy;
     } else if (ball.y +ball.radius == canvas.height) {
-        alert("GAME OVER!")
+        alert("GAME OVER!");
+        ball.x=100;
+        ball.y=100;
+        point = 0;
+        score.innerHTML="SCORE : 0";
     }
     ball.x += dx;
     ball.y += dy;
 }
 
-let rect = new Rectangle(175, 485, 150, 15, "blue", 60);
+let rect = new Rectangle(175, 485, 150, 15, "blue", 30);
 document.addEventListener("keyup", function(event){
-    rect.move();
-    rect.draw();
+    // rect.move();
+    // rect.draw();
     if(event.keyCode==37 && rect.x>=0){
         rect.x -= rect.speed;
     } else if(event.keyCode==39 && rect.x<=canvas.width-rect.width){
@@ -35,6 +40,7 @@ function ballvsrect(){
         dy= -dy;
     }
 }
+
 
 
 let point=0;
